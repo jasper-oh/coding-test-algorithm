83a03dc6ae5cad6183f05f3050641bae43362258
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-01-01 17:46:22 +0900

    Changes array name

--- 02_implement/implement_01.py
+++ 02_implement/implement_01.py
@@ -1,20 +1,20 @@
+# LEFT & RIGHT & UP & DOWN
+# where the x,y will be move ?
 n = int(input())
 x, y = 1, 1
 plans = input().split()
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-move_types = ['L', 'R', 'U', 'D']
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+moves = ['L', 'R', 'U', 'D']
 
 for plan in plans:
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
-            x_p = x + dx[i]
-            y_p = y + dy[i]
-
-    if x_p < 1 or y_p < 1 or x_p > n or y_p > n:
-
+    for i in range(len(moves)):
+        if plan == moves[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or nx > n or ny < 1 or ny > n:
         continue
-    x = x_p
-    y = y_p
-print(x_p, y_p)
+
+    x, y = nx, ny
+print(x, y)
