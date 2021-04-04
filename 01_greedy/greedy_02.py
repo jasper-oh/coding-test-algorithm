e800d6bb66eb086d7494f5f5ceb0a4ea90dc78ca
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-04 22:50:27 +0900

    improved dijkstra algo && greedy study

--- 01_greedy/greedy_02.py
+++ 01_greedy/greedy_02.py
@@ -1,34 +1,13 @@
-# n = 입력받는 배열 data 의 길이,크기
-# m = 배열을 총 더하는 횟수
-# k = 배열의 원소를 중복해서 더할수 있는 수 result = 가장 큰수를 만들어라
+#  공백으로 구분하여 값을 받아드리는 방법
+# n, m, k = map(int, input().split())
+# print("first would be", n)
+# print("type of n is", type(n))
+# print("second would be", m)
+# print("type of m is", type(m))
+# print("third would be", k)
+# print("type of k is", type(k))
 
-n, m, k = map(int, input().split())
-data = list(map(int, input().split()))
+# 공백으로 받아드리는 값을 리스트로 저장하는 방법
 
-data.sort()
-first = data[n-1]
-second = data[n-2]
-
-result = 0
-
-# while True:
-#     for i in range(k):
-#         if m == 0:
-#             break
-#         result += first
-#         m -= 1
-#     if m == 0:
-#         break
-#     result += second
-#     m -= 1
-
-count = int(m / (k+1)) * k
-count += m % (k+1)
-
-result += count * first
-result += (m-count) * second
-
-
-print(result)
-
-# BUT what if one's added number, then shoudn't be added twice..?
+# data = list(map(int, input().split()))
+# print(data)
--- 01_greedy/greedy_03.py
+++ 01_greedy/greedy_03.py
@@ -12,3 +12,12 @@ for i in range(n):
         min_value = min(min_value, a)
     result = max(result, min_value)
 print(result)
+
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value,a)
+    result = max(result, min_value)
+print(result)
