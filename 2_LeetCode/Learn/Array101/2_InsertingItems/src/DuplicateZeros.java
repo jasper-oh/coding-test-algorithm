7c42ff7385c37a24c024b3e229873275fcc71656
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-16 00:01:35 +0900

    LeetCode

---
+++ 2_LeetCode/Learn/Array101/2_InsertingItems/src/DuplicateZeros.java
@@ -0,0 +1,8 @@
+/*
+    Input: [1,0,2,3,0,4,5,0]
+    Output: null
+    Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+*/
+public class DuplicateZeros {
+
+}
--- 2_LeetCode/Learn/Array101/2_InsertingItems/src/Main.java
+++ 2_LeetCode/Learn/Array101/2_InsertingItems/src/Main.java
@@ -1,16 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-    /*
-    Input: [1,0,2,3,0,4,5,0]
-    Output: null
-    Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
-    * */
+
+        DuplicateZeros dz = new DuplicateZeros();
 
     }
 }
 
-class DuplicateZeros {
-    public void duplicateZeros(int[] arr) {
 
-    }
-}
