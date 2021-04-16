6a16a22575e257c1b36f84e035c185bbf038b8a1
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-16 15:01:23 +0900

    LeetCode

--- 2_LeetCode/TopInterviewQuestions/1_Array/src/Main.java
+++ 2_LeetCode/TopInterviewQuestions/1_Array/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        RemoveDuplicates rd = new RemoveDuplicates();
+        int answer = rd.removeDuplicates(nums);
+        System.out.println(answer);
+
     }
 }
--- 2_LeetCode/TopInterviewQuestions/1_Array/src/RemoveDuplicates.java
+++ 2_LeetCode/TopInterviewQuestions/1_Array/src/RemoveDuplicates.java
@@ -1,3 +1,25 @@
+/*
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2]
+Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the returned length.
+*/
+
+
 public class RemoveDuplicates {
 
+    int removeDuplicates(int[] nums) {
+        if (nums.length < 1) {
+            return nums.length;
+        }
+        int previousValue = Integer.MIN_VALUE;
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != previousValue) {
+                nums[k] = nums[i];
+                k++;
+            }
+            previousValue = nums[i];
+        }
+        return k;
+    }
 }
