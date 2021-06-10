0dd1ea6131a9a5426e9d39464a61da6f8fc97258
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-06-10 18:19:20 +0900

    merge

--- 1_CodingTestStart/04_sort/sort_exercise.py
+++ 1_CodingTestStart/04_sort/sort_exercise.py
@@ -11,7 +11,7 @@ for i in range(len(array)):
     for j in range(i+1, len(array)):
         if array[min_index] > array[j]:
             min_index = j
-    # SWAP
+    # SWAPPING
     array[i], array[min_index] = array[min_index], array[i]
 
 print(array)
